5a0e7bd6d06f59d8e572c19d8e47c23508159cd6
sunim thapa <thapasunim@gmail.com>
2026-09-19 14:52:21 +0545

    database connection and error message added

--- config/database.js
+++ config/database.js
@@ -1,15 +1,23 @@
-
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',          
-  password: '',          
-  database: 'club_management_system', 
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "club_management_system",
   waitForConnections: true,
-  connectionLimit: 10,   
-  queueLimit: 0
-}, console.log('Connected to the MySQL database'));
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Database connection failed:", err.message);
+  } else {
+    console.log("Connected to the MySQL database");
+    connection.release(); 
+  }
+});
 
 const db = pool.promise();
 
