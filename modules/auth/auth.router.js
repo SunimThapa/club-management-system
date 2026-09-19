69ee75ba30dc55cc051d881bc9fd2224e41f0cd1
sunim thapa <thapasunim@gmail.com>
2026-09-19 15:02:44 +0545

    added login and logout routes

--- modules/auth/auth.router.js
+++ modules/auth/auth.router.js
@@ -1,6 +1,10 @@
 const authRouter = require('express').Router();
 const authCtrl = require('./auth.controller');
+const { checkLogin } = require('../../middlewares/auth.middleware')
 
-authRouter.post('/auth/register', authCtrl.registerUser);
+
+authRouter.post('/register', authCtrl.registerUser);
+authRouter.post('/login', authCtrl.login);
+authRouter.post('/logout', checkLogin(), authCtrl.logout);
 
 module.exports = authRouter;
